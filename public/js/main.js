3be7e87287b90e29473a76e36e871de4057a1bb2
Luke Campbell <luke.s.campbell@gmail.com>
2016-12-12 11:13:36 -0500

    Dockerizes the project

--- public/js/main.js
+++ public/js/main.js
@@ -1,46 +1,53 @@
 $(function() {
-	const CATALOG_URL = "https://data.ioos.us/dataset?q=";
+  var CATALOG_URL = "https://data.ioos.us/dataset?q=";
 
-    headerScroll = function () {
-    	var scroll = $(this).scrollTop();
-        if (scroll > 50) {
-        	$('header > div > a').css('visibility', 'hidden');
-        	$('header').css('position', 'fixed');
-        	if ($('body').width() > 768) {
-            	$('header').css('top', '-50px');
-            	$('#grid-container').css('margin-top', '200px');
-        	}
-        } else {
-        	$('header > div > a').css('visibility', 'visible');
-            $('header').css({'position':'static','top':'0'});
-            if ($('body').width() > 768)
-				$('#grid-container').css('margin-top', '25px');
+  headerScroll = function () {
+    var scroll = $(this).scrollTop();
+      if (scroll > 50) {
+        $('header > div > a').css('visibility', 'hidden');
+        $('header').css('position', 'fixed');
+        if ($('body').width() > 768) {
+            $('header').css('top', '-50px');
+            $('#grid-container').css('margin-top', '200px');
         }
+      } else {
+        $('header > div > a').css('visibility', 'visible');
+          $('header').css({'position':'static','top':'0'});
+          if ($('body').width() > 768)
+      $('#grid-container').css('margin-top', '25px');
+      }
+  };
+  $(document).scroll(headerScroll);
+  window.onresize = function (e) {
+    if ($('body').width() > 768 && $('hdeader').css('top') === undefined) {
+      headerScroll();
     }
-	$(document).scroll(headerScroll);
-	window.onresize = function (e) {
-		if ($('body').width() > 768 && $('hdeader').css('top') === undefined)
-			headerScroll();
-	}
-    $('[data-toggle="tooltip"]').tooltip();
-	$('.collapse')
-		.collapse('hide')
-		.on('show.bs.collapse', () => {
-			$('#search-data-button').addClass('open');
-			setTimeout(() => {$('#search-input-collapse input').focus()}, 50);
-		})
-		.on('hide.bs.collapse', () => {
-			$('#search-data-button').removeClass('open');
-			setTimeout(() => {$('#search-data-button').blur()}, 50);
-		});
-	function searchCatalog () {
-		let query = $('input').val().trim();
-		if (query !== "")
-			window.open(CATALOG_URL + query, "_blank");
-	}
-	$('#search-input-collapse input').on('keyup', (e) => {
-		if (e.which === 13)
-			searchCatalog();
-	});
-	$('#search-input-collapse button').on('click', searchCatalog);
+  };
+  $('[data-toggle="tooltip"]').tooltip();
+  $('.collapse')
+    .collapse('hide')
+    .on('show.bs.collapse', function() {
+      $('#search-data-button').addClass('open');
+      setTimeout(function() {
+        $('#search-input-collapse input').focus();
+      }, 50);
+    })
+  .on('hide.bs.collapse', function() {
+    $('#search-data-button').removeClass('open');
+    setTimeout(function() {
+      $('#search-data-button').blur();
+    }, 50);
+  });
+  function searchCatalog () {
+    var query = $('input').val().trim();
+    if (query !== "") {
+      window.open(CATALOG_URL + query, "_blank");
+    }
+  }
+  $('#search-input-collapse input').on('keyup', function(e) {
+    if (e.which === 13) {
+      searchCatalog();
+    }
+  });
+  $('#search-input-collapse button').on('click', searchCatalog);
 });
